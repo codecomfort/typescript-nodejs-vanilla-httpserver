13d10b77f3008a4199ec7e15123dcfb8f6a1094c
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2018-06-02 18:14:05 +0900

    add listeningListener

--- src/serverModule.ts
+++ src/serverModule.ts
@@ -1,11 +1,16 @@
 import * as http from "http";
 
 export class HttpServer {
+  private port: number = 5000;
   public init = () => {
     const listenr = (request, response) => this.requestHandler(request, response);
     const server: http.Server = http.createServer(listenr);
-    const port: number = 5000;
-    server.listen(port);
+    const listeningListener = () => this.listeningHandler();
+    server.listen(this.port, this.listeningHandler);
+  }
+
+  private listeningHandler = () => {
+    console.log(`${this.port} で http サーバーが待ち受け状態です`);
   }
 
   private requestHandler = (request: http.IncomingMessage, response: http.ServerResponse) => {
