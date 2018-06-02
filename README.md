11581587015b78abfe23b23460c57839ed8173de
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2018-06-02 15:03:35 +0900

    add README.md, import http

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# Usage
+```
+npm run build
+node src/app
+```
+then
+http://localhost:5000
+
+# External links
+[TypeScriptで始めるNode.js入門 - ICS MEDIA](https://ics.media/entry/4682)
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log("Hello! Node.js + TypeScript");
+import * as http from "http";
+
+class Main {
+  constructor() {
+    const listenr = (request, response) => this.requestHandler(request, response);
+    const server: http.Server = http.createServer(listenr);
+    const port: number = 5000;
+    server.listen(port);
+  }
+
+  private requestHandler = (request: http.IncomingMessage, response: http.ServerResponse): void => {
+    const chunk = 'Hello Node.js with Typescript';
+    response.end(chunk);
+  }
+}
+
+const main = new Main();
